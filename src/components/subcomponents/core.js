b3fe0df1b6f8989549814c355dd52eed95b198e6
joelchitala <chitalahjoel@gmail.com>
2025-07-26 01:33:58 +0300

    updates

--- src/components/subcomponents/core.js
+++ src/components/subcomponents/core.js
@@ -2,241 +2,224 @@ import { comparison_mapper } from "./comparison/comparison_mapper.js";
 import { comparison_patcher } from "./comparison/comparison_patcher.js";
 import { generateUUID } from "../../shared/utils.js";
 
-
 export const REBUILD_TYPES = {
-    none: "none",
-    partial: "partial",
-    full: "full",
-}
+	none: "none",
+	partial: "partial",
+	full: "full",
+};
 
 export const ACTION_TYPES = {
-    none:"none",
-    add:"add",
-    edit:"edit",
-    delete:"delete",
-}
+	none: "none",
+	add: "add",
+	edit: "edit",
+	delete: "delete",
+};
 
 export const COMPONENT_TYPES = {
-    base_component:"base_component",
-    widget:"widget",
+	base_component: "base_component",
+	widget: "widget",
 
-    no_child_widget:"no_child_widget",
-    single_child_widget:"single_child_widget",
-    multi_child_widget:"multi_child_widget",
+	no_child_widget: "no_child_widget",
+	single_child_widget: "single_child_widget",
+	multi_child_widget: "multi_child_widget",
 
-    state_type_widget:"state_type_widget",
-    stateless_widget:"stateless_widget",
-    stateful_widget:"stateful_widget",
-}
-
+	state_type_widget: "state_type_widget",
+	stateless_widget: "stateless_widget",
+	stateful_widget: "stateful_widget",
+};
 
 export class BaseComponent {
-    constructor({key, body, attrs = {}} = {}) {
+	constructor({ key, body, attrs = {} } = {}) {
+		this.data = {
+			key: key,
+			widget_id: generateUUID(),
+			body: body,
+			attrs: attrs,
+			component_type: COMPONENT_TYPES.base_component,
+		};
+		this.bind();
+	}
 
-        this.data = {
-            key: key,
-            widget_id: generateUUID(),
-            body: body,
-            attrs: attrs,
-            component_type: COMPONENT_TYPES.base_component,
-        }
-        this.bind()
+	bind() {
+		const body = this.data.body;
+		const attrs = this.data.attrs;
 
-    }
+		body.setAttribute("data-widget_id", this.data.widget_id);
 
-    bind(){
-        const body = this.data.body;
-        const attrs = this.data.attrs;
+		if (attrs) {
+			const keys = Object.keys(attrs);
 
-        body.setAttribute('data-widget_id',this.data.widget_id);
+			for (let i = 0; i < keys.length; i++) {
+				const key = keys[i];
+				const value = attrs[key];
+				body[key] = value;
+			}
+		}
+	}
 
-        if(attrs){
-            const keys = Object.keys(attrs);
+	getKey = () => this.data.key;
 
-            for (let i = 0; i < keys.length; i++) {
-                const key = keys[i];
-                const value = attrs[key];
-                body[key] = value;
-            }
-        }
-    }
+	render(build) {
+		const body = this.data.body;
 
-    getKey = () => this.data.key;
+		const context_id = generateUUID();
+		body.setAttribute("data-context_id", context_id);
 
-    render(build){
-        const body = this.data.body;
+		if (!build) return body;
 
-        const context_id = generateUUID();
-        body.setAttribute("data-context_id",context_id);
+		body.innerHTML = "";
 
-        if(!build) return body;
-        
-        body.innerHTML = "";
-
-        return body;
-    }
+		return body;
+	}
 }
 
-export class Widget extends BaseComponent{
-    constructor({key, body, attrs = {}, template = (self,body) =>{}} = {}) {
-        super({key:key, body:body, attrs:attrs});
+export class Widget extends BaseComponent {
+	constructor({ key, body, attrs = {}, template = (self, body) => {} } = {}) {
+		super({ key: key, body: body, attrs: attrs });
 
-        this.data = {
-            ...this.data,
-            template:template,
-            component_type: COMPONENT_TYPES.widget,
-        }
-    }
+		this.data = {
+			...this.data,
+			template: template,
+			component_type: COMPONENT_TYPES.widget,
+		};
+	}
 
-    render(build){
-        const body = super.render(build);
+	render(build) {
+		const body = super.render(build);
 
-        if(build) {
-            this.data.template(this, body);
-        }
+		if (build) {
+			this.data.template(this, body);
+		}
 
-        return body;
-    }
+		return body;
+	}
 }
 
-export class NoChildWidget extends Widget{
-    constructor({key, body, attrs = {}, template = (self,body) =>{}} = {}) {
-        super({key:key, body:body, attrs:attrs});
+export class NoChildWidget extends Widget {
+	constructor({ key, body, attrs = {}, template = (self, body) => {} } = {}) {
+		super({ key: key, body: body, attrs: attrs });
 
-        this.data = {
-            ...this.data,
-            template:template,
-            component_type: COMPONENT_TYPES.no_child_widget,
-        }
-    }
-
-    
+		this.data = {
+			...this.data,
+			template: template,
+			component_type: COMPONENT_TYPES.no_child_widget,
+		};
+	}
 }
 
-export class MultiChildWidget extends Widget{
-    constructor(
-        {
-            key, 
-            body, 
-            attrs = {}, 
-            children = [], 
-            template = (self,body,children) =>{}
-        } = {}
-    ){
-        
-        super({key:key, body:body, attrs: attrs, template:(self,body) =>{
-                template(self,body,children)
-            }
-        });
+export class MultiChildWidget extends Widget {
+	constructor({
+		key,
+		body,
+		attrs = {},
+		children = [],
+		template = (self, body, children) => {},
+	} = {}) {
+		super({
+			key: key,
+			body: body,
+			attrs: attrs,
+			template: (self, body) => {
+				template(self, body, children);
+			},
+		});
 
-        this.data = {
-            ...this.data,
-            children:children,
-            component_type: COMPONENT_TYPES.multi_child_widget,
-        }
-    }
+		this.data = {
+			...this.data,
+			children: children,
+			component_type: COMPONENT_TYPES.multi_child_widget,
+		};
+	}
 }
 
-export class SingleChildWidget extends Widget{
-    constructor(
-        {
-            key, 
-            body, 
-            attrs = {}, 
-            child, 
-            template = (self,body,child) =>{}
-        } = {}
-    ) {
-        super({key:key, body:body, attrs: attrs, template:(self,body)=>{
-                template(self,body,child);
-            }
-        });
-        this.data = {
-            ...this.data,
-            child:child,
-            component_type: COMPONENT_TYPES.single_child_widget,
-        }
-    }
+export class SingleChildWidget extends Widget {
+	constructor({
+		key,
+		body,
+		attrs = {},
+		child,
+		template = (self, body, child) => {},
+	} = {}) {
+		super({
+			key: key,
+			body: body,
+			attrs: attrs,
+			template: (self, body) => {
+				template(self, body, child);
+			},
+		});
+		this.data = {
+			...this.data,
+			child: child,
+			component_type: COMPONENT_TYPES.single_child_widget,
+		};
+	}
 }
 
-export class StateTypeWidget extends BaseComponent{
-    constructor({
-        key, 
-        attrs = {}, 
-        builder = (self) => {}
-    } = {} ){
-        const body = document.createElement('section');
-        super({key:key, body:body, attrs:attrs});
+export class StateTypeWidget extends BaseComponent {
+	constructor({ key, attrs = {}, builder = (self) => {} } = {}) {
+		const body = document.createElement("section");
+		super({ key: key, body: body, attrs: attrs });
 
-        this.data = {
-            ...this.data,
-            builder:builder,
-            widget:builder(this),
-            component_type: COMPONENT_TYPES.state_type_widget,
-        }
-    }
+		this.data = {
+			...this.data,
+			builder: builder,
+			widget: builder(this),
+			component_type: COMPONENT_TYPES.state_type_widget,
+		};
+	}
 
-    build(){
-        const widget = this.data.builder(this);
-        this.data.widget = widget;
+	build() {
+		const widget = this.data.builder(this);
+		this.data.widget = widget;
 
-        return widget
-    }
+		return widget;
+	}
 
-    render(build){
-        const body = super.render(build);
+	render(build) {
+		const body = super.render(build);
 
-        if(build) {
-            const widget = this.data.widget
-            body.appendChild(widget.render(build));
-        }
+		if (build) {
+			const widget = this.data.widget;
+			body.appendChild(widget.render(build));
+		}
 
-        return body;
-    }
+		return body;
+	}
 }
 
-export class StatelessWidget extends StateTypeWidget{
-    constructor({
-        key, 
-        attrs = {}, 
-        builder = (self) => {}
-    } = {}){
-        super({key:key, attrs:attrs, builder: builder});
+export class StatelessWidget extends StateTypeWidget {
+	constructor({ key, attrs = {}, builder = (self) => {} } = {}) {
+		super({ key: key, attrs: attrs, builder: builder });
 
-        this.data = {
-            ...this.data,
-            component_type: COMPONENT_TYPES.stateless_widget,
-        }
-
-    }
+		this.data = {
+			...this.data,
+			component_type: COMPONENT_TYPES.stateless_widget,
+		};
+	}
 }
 
-export class StatefulWidget extends StateTypeWidget{
-    constructor({
-        key, 
-        attrs = {}, 
-        builder = (self) => {}
-    } = {}){
-        super({key:key, attrs:attrs, builder: builder});
+export class StatefulWidget extends StateTypeWidget {
+	constructor({ key, attrs = {}, builder = (self) => {} } = {}) {
+		super({ key: key, attrs: attrs, builder: builder });
 
-        this.data = {
-            ...this.data,
-            component_type: COMPONENT_TYPES.stateful_widget,
-        }
-    }
+		this.data = {
+			...this.data,
+			component_type: COMPONENT_TYPES.stateful_widget,
+		};
+	}
 
-    setState(fn = () =>{}){
-        const widget = this.data.widget;
-        const new_widget = this.data.builder(this);
+	setState(fn = () => {}) {
+		if (typeof fn == "function") fn();
 
-        const mapped_comparison = comparison_mapper({
-            component_1:widget, 
-            component_2:new_widget
-        });
+		const widget = this.data.widget;
+		const new_widget = this.data.builder(this);
 
-        comparison_patcher(mapped_comparison["comparison_dict"]);
-        
-        fn();
-    }
+		const mapped_comparison = comparison_mapper({
+			component_1: widget,
+			component_2: new_widget,
+		});
 
+		comparison_patcher(mapped_comparison["comparison_dict"]);
+	}
 }
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,15 +1,16 @@
 export const generateUUID = (max = 100000, length = 16) => {
-    let uuid = '';
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (let i = 0; i < length; i++) {
-        let randomValue = crypto.getRandomValues(new Uint8Array(1))[0] % chars.length;
-        uuid += chars[randomValue];
-    }
-    
-    return uuid + '-' + Math.floor(Math.random() * max).toString();
-}
+	let uuid = "";
+	const chars =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
+	for (let i = 0; i < length; i++) {
+		let randomValue =
+			crypto.getRandomValues(new Uint8Array(1))[0] % chars.length;
+		uuid += chars[randomValue];
+	}
+
+	return uuid + "-" + Math.floor(Math.random() * max).toString();
+};
 
 // export const smallerArray = (array_1 = [], array_2 = []) =>{
 //     const data = {
@@ -28,172 +29,186 @@ export const generateUUID = (max = 100000, length = 16) => {
 //     return data;
 // }
 
-export const padArray = (array, num = 0, pad_obj = null) =>{
-    
-    for (let i = 0; i < num; i++) {
-        array.push(pad_obj)
-    }
+export const padArray = (array, num = 0, pad_obj = null) => {
+	for (let i = 0; i < num; i++) {
+		array.push(pad_obj);
+	}
 
-    return array;
-}
+	return array;
+};
 
-export const moveElement = (array, fromIndex, toIndex) =>{
-    if(toIndex < 0) return;
+export const moveElement = (array, fromIndex, toIndex) => {
+	if (toIndex < 0) return;
 
-    const [element] = array.splice(fromIndex, 1);
+	const [element] = array.splice(fromIndex, 1);
 
-    array.splice(toIndex, 0, element);
+	array.splice(toIndex, 0, element);
 
-    return array;
-}
+	return array;
+};
 
-export const  dictSimilar = (obj1, obj2) =>{
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
+export const dictSimilar = (obj1, obj2) => {
+	const keys1 = Object.keys(obj1);
+	const keys2 = Object.keys(obj2);
 
-    if (keys1.length !== keys2.length) {
-        return false;
-    }
+	if (keys1.length !== keys2.length) {
+		return false;
+	}
 
-    for (const key of keys1) {
-        if (obj1[key] !== obj2[key]) {
-            return false; 
-        }
-    }
+	for (const key of keys1) {
+		if (obj1[key] !== obj2[key]) {
+			return false;
+		}
+	}
 
-    return true; 
-}
+	return true;
+};
 
-export const dictSimilarDeep = (obj1, obj2) =>{
-    if (typeof obj1 !== "object" || typeof obj2 !== "object" || obj1 === null || obj2 === null) {
-        return obj1 === obj2;
-    }
+export const dictSimilarDeep = (obj1, obj2) => {
+	if (
+		typeof obj1 !== "object" ||
+		typeof obj2 !== "object" ||
+		obj1 === null ||
+		obj2 === null
+	) {
+		return obj1 === obj2;
+	}
 
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
+	const keys1 = Object.keys(obj1);
+	const keys2 = Object.keys(obj2);
 
-    if (keys1.length !== keys2.length) {
-        return false;
-    }
+	if (keys1.length !== keys2.length) {
+		return false;
+	}
 
-    for (const key of keys1) {
-        if (!areObjectsSimilarDeep(obj1[key], obj2[key])) {
-            return false;
-        }
-    }
+	for (const key of keys1) {
+		if (!areObjectsSimilarDeep(obj1[key], obj2[key])) {
+			return false;
+		}
+	}
 
-    return true;
-}
+	return true;
+};
 
-export const areObjectsSimilarDeep = (obj1, obj2) =>{
-    // Handle primitive types, null, and functions
-    if (typeof obj1 !== "object" || typeof obj2 !== "object" || obj1 === null || obj2 === null) {
-        if (typeof obj1 === "function" && typeof obj2 === "function") {
-            return obj1.toString() === obj2.toString(); // Compare function code
-        }
-        return obj1 === obj2; // Compare primitive values
-    }
+export const areObjectsSimilarDeep = (obj1, obj2) => {
+	// Handle primitive types, null, and functions
+	if (
+		typeof obj1 !== "object" ||
+		typeof obj2 !== "object" ||
+		obj1 === null ||
+		obj2 === null
+	) {
+		if (typeof obj1 === "function" && typeof obj2 === "function") {
+			return obj1.toString() === obj2.toString(); // Compare function code
+		}
+		return obj1 === obj2; // Compare primitive values
+	}
 
-    // Handle Arrays
-    if (Array.isArray(obj1) && Array.isArray(obj2)) {
-        if (obj1.length !== obj2.length) {
-            return false; // Different array lengths
-        }
+	// Handle Arrays
+	if (Array.isArray(obj1) && Array.isArray(obj2)) {
+		if (obj1.length !== obj2.length) {
+			return false; // Different array lengths
+		}
 
-        for (let i = 0; i < obj1.length; i++) {
-            if (!areObjectsSimilarDeep(obj1[i], obj2[i])) {
-                return false; // Element mismatch
-            }
-        }
+		for (let i = 0; i < obj1.length; i++) {
+			if (!areObjectsSimilarDeep(obj1[i], obj2[i])) {
+				return false; // Element mismatch
+			}
+		}
 
-        return true; // Arrays are identical
-    }
+		return true; // Arrays are identical
+	}
 
-    // If one is an array and the other is not, they are different
-    if (Array.isArray(obj1) !== Array.isArray(obj2)) {
-        return false;
-    }
+	// If one is an array and the other is not, they are different
+	if (Array.isArray(obj1) !== Array.isArray(obj2)) {
+		return false;
+	}
 
-    // Handle Objects
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
+	// Handle Objects
+	const keys1 = Object.keys(obj1);
+	const keys2 = Object.keys(obj2);
 
-    if (keys1.length !== keys2.length) {
-        return false; // Different number of keys
-    }
+	if (keys1.length !== keys2.length) {
+		return false; // Different number of keys
+	}
 
-    for (const key of keys1) {
-        if (!keys2.includes(key) || !areObjectsSimilarDeep(obj1[key], obj2[key])) {
-            return false; // Key mismatch or value mismatch
-        }
-    }
+	for (const key of keys1) {
+		if (!keys2.includes(key) || !areObjectsSimilarDeep(obj1[key], obj2[key])) {
+			return false; // Key mismatch or value mismatch
+		}
+	}
 
-    return true; // Objects are identical
-}
+	return true; // Objects are identical
+};
 
 export function createSelfDestructingObject(obj) {
-    return new Proxy(obj, {
-      get(target, prop, receiver) {
-        if (prop in target) {
-          const value = target[prop];
-  
-          // If the value is an object, make it self-destructing
-          if (value && typeof value === 'object' && !Array.isArray(value)) {
-            const nestedProxy = createSelfDestructingObject(value);
-  
-            // Check if the nested object still has keys after access
-            if (Object.keys(value).length === 0) {
-              delete target[prop]; // Remove the parent property if the nested object is empty
-            }
-  
-            return nestedProxy;
-          }
-  
-          // For non-objects, simply remove the property
-          delete target[prop];
-          return value;
-        }
-        return undefined; // Return undefined if the property doesn't exist
-      },
-    });
-  }
+	return new Proxy(obj, {
+		get(target, prop, receiver) {
+			if (prop in target) {
+				const value = target[prop];
 
-export function deepCopy(obj) {
-    if (obj === null || typeof obj !== "object") return obj;
+				// If the value is an object, make it self-destructing
+				if (value && typeof value === "object" && !Array.isArray(value)) {
+					const nestedProxy = createSelfDestructingObject(value);
 
-    // Handle Array
-    if (Array.isArray(obj)) {
-        return obj.map(item => deepCopy(item));
-    }
+					// Check if the nested object still has keys after access
+					if (Object.keys(value).length === 0) {
+						delete target[prop]; // Remove the parent property if the nested object is empty
+					}
 
-    // Handle Object
-    const copy = {};
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            copy[key] = deepCopy(obj[key]);
-        }
-    }
-    return copy;
+					return nestedProxy;
+				}
+
+				// For non-objects, simply remove the property
+				delete target[prop];
+				return value;
+			}
+			return undefined; // Return undefined if the property doesn't exist
+		},
+	});
 }
 
-export function replaceOrAppendNestedEntries({obj, replacements, append_mode = false}) {
-    for (const [key, value] of Object.entries(replacements)) {
-      if (typeof value === 'object' && value !== null && !(value instanceof Array)) {
-        // If the replacement value is an object, recurse into the corresponding nested object
-        if (key in obj && typeof obj[key] === 'object' && obj[key] !== null) {
-          replaceOrAppendNestedEntries(obj[key], value);
-        } else {
-          obj[key] = value; // If the key doesn't exist, add the replacement object
-        }
-      } else {
-        // Replace or add the value directly
+export function deepCopy(obj) {
+	if (obj === null || typeof obj !== "object") return obj;
 
-        if (append_mode) {
-            obj[key] += value;
-        }else{
-            obj[key] = value;
-        }
-      }
-    }
-    return obj;
+	// Handle Array
+	if (Array.isArray(obj)) {
+		return obj.map((item) => deepCopy(item));
+	}
+
+	// Handle Object
+	const copy = {};
+	for (const key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			copy[key] = deepCopy(obj[key]);
+		}
+	}
+	return copy;
+}
+
+export function replaceOrAppendNestedEntries({
+	obj,
+	replacements,
+	append_mode = false,
+}) {
+	for (const [key, value] of Object.entries(replacements)) {
+		if (
+			typeof value === "object" &&
+			value !== null &&
+			!(value instanceof Array)
+		) {
+			if (key in obj && typeof obj[key] === "object" && obj[key] !== null) {
+				replaceOrAppendNestedEntries(obj[key], value);
+			} else {
+				obj[key] = value;
+			}
+		} else {
+			if (append_mode) {
+				obj[key] += value;
+			} else {
+				obj[key] = value;
+			}
+		}
+	}
+	return obj;
 }
